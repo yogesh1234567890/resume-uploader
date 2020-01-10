1d98dbd1f7e6df224c9a060ccde525fc0567c37d
yogesh1234567890 <yogeshbhattarai073@gmail.com>
2020-01-10 18:13:05 +0545

    checking uploading

--- resume/views.py
+++ resume/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import request,HttpResponse
+from resume.models import Resume
+from resume.forms import *
 
-# Create your views here.
+def index(request):
+    if request.method=="POST":
+        form=ResumeForm(request.POST, request.FILES)
+
+        if(form.is_valid()):
+            form.save()
+            return redirect('/fronts')
+    else:
+        form=ResumeForm()
+    return render(request,'index.html',{'form':form})
+
+def fronts(request):
+    return render(request,'display.html',{
+            'display':Resume.objects.all(), })
