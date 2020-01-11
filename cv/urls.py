033835f2f51e69006fc0cf1eddc5370a13d5e3ed
yogesh1234567890 <yogeshbhattarai073@gmail.com>
2020-01-11 14:47:03 +0545

    working on displaying data

--- cv/urls.py
+++ cv/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from resume.views import index,fronts
+from resume.views import index,fronts,file
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index),
-    path('fronts/',fronts),
+    path('fronts/',file),
 ]
--- resume/views.py
+++ resume/views.py
@@ -1,19 +1,36 @@
-from django.shortcuts import render,redirect
-from django.http import request,HttpResponse
+from django.shortcuts import render, redirect
+from django.http import request, HttpResponse
 from resume.models import Resume
+from PyPDF2 import PdfFileReader
 from resume.forms import *
+from files.files import *
+import datetime
+
+
 
 def index(request):
-    if request.method=="POST":
-        form=ResumeForm(request.POST, request.FILES)
+    if request.method == "POST":
+        form = ResumeForm(request.POST, request.FILES)
 
         if(form.is_valid()):
             form.save()
+
             return redirect('/fronts')
     else:
-        form=ResumeForm()
-    return render(request,'index.html',{'form':form})
+        form = ResumeForm()
+    return render(request, 'index.html', {'form': form})
+
 
 def fronts(request):
-    return render(request,'display.html',{
-            'display':Resume.objects.all(), })
+    return render(request, 'display.html', {
+        'display': Resume.objects.all(), })
+
+
+def file(request):
+    pdfFileObj = open('/home/yogesh/Desktop/cv/files/files/sample.pdf', 'rb')
+    pdfReader = PdfFileReader(pdfFileObj)
+    pageObj = pdfReader.getPage(0)
+    text=pageObj.extractText()
+    return render(request, 'display.html', {
+    'display': text.split(' '),
+})
